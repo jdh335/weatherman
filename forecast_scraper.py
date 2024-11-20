fe16ded66433e3d7375bdaaa53373441d0223fdc
Marcos <edwardmarcos717@gmail.com>
2024-11-20 12:46:18 -0700

    updated to Rexburg

--- forecast_scraper.py
+++ forecast_scraper.py
@@ -2,7 +2,7 @@ from requests_html import HTMLSession
 
 s = HTMLSession()
 
-query = 'lima'
+query = 'Rexburg'
 url = f'https://www.google.com/search?q=weather+{query}'
 
 r= s.get(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'})
